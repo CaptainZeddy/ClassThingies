e6b065acf542d441e6b35e674f55819fb1092957
vespertino <vespertino@max12>
2025-10-01 19:09:08 +0200

    ejercicio 7 de servicios (ni puta idea)

--- servicios y procesos/RA1/ServiciosYProcesos/src/sumaDiferencias/SumaBuilder.java
+++ servicios y procesos/RA1/ServiciosYProcesos/src/sumaDiferencias/SumaBuilder.java
@@ -7,22 +7,40 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class SumaBuilder {
-	
+
 	public static void main(String[] args)throws IOException {
 
 		File f= new File("bin");
-		
-		ProcessBuilder pb= new ProcessBuilder("java","sumaDiferencias.SumaMain","Dani");
-		
+
+		ProcessBuilder pb= new ProcessBuilder("java","sumaDiferencias.SumaMain","2","4");
+
+		ProcessBuilder cl = new ProcessBuilder("sh");
+
+		cl.directory(f);
 		pb.directory(f);
+		
 		System.out.println("Ruta completa: "+pb.directory());
 		
+		File fOut = new File ("salida.txt");;
+		File fErr = new File ("error.txt");;
+		File fBat = new File ("fichero.txt");;
+		
+		cl.redirectInput(ProcessBuilder.Redirect.from(fBat));
+		cl.redirectOutput(ProcessBuilder.Redirect.to(fOut));
+		cl.redirectError(ProcessBuilder.Redirect.appendTo(fErr));
+
+		
+		Process a = cl.start();
 		Process p = pb.start();
+
+
+
+
+	
 		
-		
-		
+
 		try {
-			InputStream is = p.getInputStream();
+			InputStream is = a.getInputStream();
 			int c;
 			while ((c = is.read()) != -1)
 				System.out.print((char) c);
@@ -30,7 +48,7 @@ public class SumaBuilder {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		int exitVal;
 		try {
 			exitVal = p.waitFor();
--- servicios y procesos/RA1/ServiciosYProcesos/src/sumaDiferencias/SumaMain.java
+++ servicios y procesos/RA1/ServiciosYProcesos/src/sumaDiferencias/SumaMain.java
@@ -4,7 +4,16 @@ public class SumaMain {
 
 	public static void main(String[] args) {
 
-		System.out.println(args[0]);
+		int num1= Integer.parseInt(args[0]);
+		int num2= Integer.parseInt(args[1]);
+		int res=0;
+		while(num1<=num2) {
+			res+=num1;
+			num1++;
+			
+		}
+		System.out.println(res);
+		System.exit(0);
 	}
 
 }
