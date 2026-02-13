8ee86d394c17172e168074556c658c49a7e1ee9b
vespertino <vespertino@max12>
2026-02-13 17:17:14 +0100

    ULTIMO PUSH DEL AÑO JAJAJ GOD

---
+++ servicios y procesos/RA5/UT5_Ejemplos/src/Numeritos.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Numeritos {
+	private static Scanner scan= new Scanner(System.in);
+
+
+	public static void main(String[] args) {
+		if(scan.hasNextLine()) {
+			String matricula = scan.nextLine();
+			char[] matriculaChar = matricula.toCharArray();			
+			for(int i = 0; i < matriculaChar.length;i++) {
+				if (matriculaChar.) {
+					
+				}
+			}
+		}
+	}
+
+}
